f96e3a3362fa2c51cebb25171d85401581f39a08
Oskar <oskar.orion18@gmail.com>
2024-06-28 00:18:53 +0200

    usunięcie niepotrzebnych plików

--- wypełnianie/czesci.py
+++ wypełnianie/czesci.py
@@ -1,8 +1,5 @@
 import pandas as pd
-import numpy as np
 import os
-import matplotlib.pyplot as plt
-import scipy.stats as stats
 
 def get_data_parts():
     data_parts = pd.read_csv(os.path.dirname(__file__) + '/inputs/czesci_pl.csv')
